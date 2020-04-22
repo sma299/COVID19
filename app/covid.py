0be51522683219eebe0df1936842dc271d8927de
sma299 <59669463+sma299@users.noreply.github.com>
2020-04-21 21:37:29 -0400

    added in a functional iterator that prints out the deaths based on the county!

--- app/covid.py
+++ app/covid.py
@@ -5,7 +5,12 @@ import os
 import datetime
 import csv
 import urllib.request
-import pandas as pd
+
+# ask user to input a county
+print("Welcome to the COVID-19 county tracker.")
+print("--------------------------------------------------")
+
+county_input = input("Please input your state county here: ") #the resulting value is a string
 
 # this is the url that contains all of the county information
 url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
@@ -17,8 +22,17 @@ data = response.read()
 # using the nytdata in data folder to store information
 file_name = os.path.join(os.path.dirname(__file__),"..", "data", "nytdata.csv")
 
+# write the data to a file
 with open(file_name, 'wb') as f:
     f.write(data)
 
-covid_dict = pd.read_csv(file_name)
+# parse through that data using the CSV module
+# ['date', 'county', 'state', 'fips', 'cases', 'deaths']
+with open(file_name, 'r') as f2:
+    csv_file_reader = csv.DictReader(f2)
+    for row in csv_file_reader:
+        if str(row["county"]) == county_input:
+            print(row["deaths"])
+
+
 
