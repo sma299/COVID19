59d26969eb2837ae48ab2095f112aeb03e64bb36
sma299 <59669463+sma299@users.noreply.github.com>
2020-05-03 19:59:18 -0400

    added new files for testing, imported functions

---
+++ test/covid_test.py
@@ -0,0 +1,3 @@
+import pytest # for pytest.raises (see: https://docs.pytest.org/en/latest/assert.html)
+
+from app.covid import get_data, string_validation, data_validation, average_deaths, average_cases, deaths_change, cases_change
