8737eca84a549b46805333288935a148a3455f22
sma299 <59669463+sma299@users.noreply.github.com>
2020-05-04 13:43:02 -0400

    removed csv import from testing file

--- test/covid_test.py
+++ test/covid_test.py
@@ -1,5 +1,4 @@
 import pytest # for pytest.raises (see: https://docs.pytest.org/en/latest/assert.html)
-import csv
 
 from app.covid import string_validation, data_validation, average_deaths, average_cases, deaths_change, cases_change
 
